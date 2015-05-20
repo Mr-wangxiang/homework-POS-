3b5bdab8be077d23a8d87626ad71be117e3c071e
Mr-wangxiang <wxthoughtworks@outlook.com>
2015-05-20 11:47:10 +0000

    add pos_v0.1/src/main.js

--- pos_v0.1/src/main.js
+++ pos_v0.1/src/main.js
@@ -1,4 +1,44 @@
 //TODO: Please write code in this file.
 function printInventory(inputs) {
+    var expectText ='***<没钱赚商店>购物清单***\n' ;
+    var array = [];
 
+    	for(var i = 0; i<inputs.length;i++)
+    	{
+    		var exist = false ;
+    		for(var j=0 ; j<array.length; j++)
+    		{
+
+    			if(array[j].name == inputs[i].name)
+    			{
+    				array[j].count++;
+    				exist = true;
+
+    			}
+
+    		}
+    		if(!exist)
+    		{
+    			var temp ={};
+    			temp.name = inputs[i].name ;
+    			temp.barcode =inputs[i].barcode;
+    			temp.unit = inputs[i].unit;
+    			temp.price = inputs[i].price.toFixed(2) ;
+    			temp.count = 1;
+    			array.push(temp);
+    		}
+
+    	}
+
+    var sum = 0;
+    	for(var i=0; i<array.length;i++){
+    		sum+=array[i].count*array[i].price;
+            expectText +='名称：'+array[i].name+'，'+'数量：'+array[i].count+array[i].unit+'，单价：'+array[i].price+'(元)，小计：'+(array[i].price*array[i].count).toFixed(2)+'(元)\n';
+
+    	}
+    expectText +='----------------------\n' +
+    	'总计：'+sum.toFixed(2)+'(元)\n' +
+
+    	'**********************'
+        console.log(expectText);
 }
--- pos_v0/src/main.js
+++ pos_v0/src/main.js
@@ -1,4 +1,17 @@
 //TODO: Please write code in this file.
 function printInventory(inputs) {
+    var expectText ='***<没钱赚商店>购物清单***\n';
+    var sum=0;
+    for(var i=0; i< inputs.length; i++){
+            //得到数组i个子集和inputs[i]
+            //取得子集和的每个元素 如：取到名称--inputs[i].name;
+            sum += inputs[i].price*inputs[i].count;
+            expectText +='名称：'+inputs[i].name+'，'+'数量：'+inputs[i].count+inputs[i].unit+'，单价：'+inputs[i].price.toFixed(2)+'(元)，小计：'+(inputs[i].price*inputs[i].count).toFixed(2)+'(元)\n';
 
+        }
+    expectText +='----------------------\n' +
+    	'总计：'+sum.toFixed(2)+'(元)\n' +
+
+    	'**********************';
+    console.log(expectText);
 }
